c0732d14c3c378920cde47c67e87957f92a89521
Yusuke Sasaki <ys.sasaki@uchida-it.co.jp>
2020-08-05 00:00:48 +0900

    1-04 studied

---
+++ AtCoder_Programming_Guide_for_beginners/1/APG4b-1-04.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 一年の秒数
+    int seconds = 365 * 24 * 60 * 60;
+
+
+    cout << seconds << endl;        //1年
+    cout << seconds * 2 << endl;    //2年
+    cout << seconds * 5 << endl;    //5年
+    cout << seconds * 10 << endl;   //10年
+}
