2cd64391495ba95b75dcdea9bd6c8dd65e3eb22e
佐々木勇輔 <ys.sasaki@uchida-it.co.jp>
2020-08-26 11:03:23 +0900

    1-12 Analogy studied

---
+++ AtCoder_Begginer_Contest/049/A-Iaiwooe_Aoiewooou.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c; // 判断文字
+    cin >> c;
+
+    switch (c) {
+        case 'a':
+            cout << "vowel" << endl;
+            break;
+        case 'e':
+            cout << "vowel" << endl;
+            break;
+        case 'i':
+            cout << "vowel" << endl;
+            break;
+        case 'o':
+            cout << "vowel" << endl;
+            break;
+        case 'u':
+            cout << "vowel" << endl;
+            break;
+        default:
+            cout << "consonant" << endl;
+    }
+}
