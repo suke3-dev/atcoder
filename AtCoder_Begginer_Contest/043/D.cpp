ef73a5dc6c8a42dbd52c0598a7010bf258dfa3d2
Yusuke Sasaki <ys.sasaki@uchida-it.co.jp>
2020-09-30 20:16:10 +0900

    retry

--- AtCoder_Begginer_Contest/043/D.cpp
+++ AtCoder_Begginer_Contest/043/D.cpp
@@ -3,53 +3,32 @@ using namespace std;
  
 int main() {
     string s;
-    string check;
-    long unsigned int checkCount;
-    int ansStart = 0;
-    int ansEnd = 0;
-    bool checkFlg = false;
     cin >> s;
 
-    // アンバランスな事象は文字列sの長さの半分以上の場合に判断できる
-    for (long unsigned int i = 0; i < s.size() / 2; i++) {
-        // 偶数と奇数で確認する文字数を調整
-        if (s.size() % 2 == 0) {
-            check = s.substr(i, (s.size() / 2) + 1);
+    if (s.size() == 2) {
+        if (s.at(0) == s.at(1)) {
+            cout << 1 << " " << 2 << endl;
+            return 0;
         }
-        else {
-            check = s.substr(i, (s.size() / 2) + 2);
+        else
+        {
+            cout << -1 << " " << -1 << endl;
+            return 0;
         }
-        checkCount = 0;
-        for (long unsigned int j = 0; j < check.size(); j++) {
-            if (check.at(0) == check.at(j)) {
-                checkCount++;
+    }
+    else {
+        for (int i = 0; i < s.size() - 1; i++) {
+            if(s.at(i) == s.at(i + 1)) {
+                cout << i + 1 << " " << i + 2 << endl;
+                return 0;
+            }
+            else if (i < s.size() - 2 && s.at(i) == s.at(i + 2)) {
+                cout << i + 1 << " " << i + 3 << endl;
+                return 0;
             }
         }
-        
-        // 偶数と奇数で確認する文字数を調整
-        if (s.size() % 2 == 0) {
-            if (checkCount >= s.size() / 2) {
-                ansStart = i + 1;
-                ansEnd = i + check.size();
-                cout << ansStart << " " << ansEnd << endl;
-                checkFlg = true;
-                break;
-            } 
-        }
-        else {
-            if (checkCount >= s.size() / 2 + 1) {
-                ansStart = i + 1;
-                ansEnd = i + check.size();
-                cout << ansStart << " " << ansEnd << endl;
-                checkFlg = true;
-                break;
-            } 
-        }
-        
-    }
-  
-    if (!checkFlg) {
-        cout << -1 << " " << -1 << endl;
     }
 
+    cout << -1 << " " << -1 << endl;
+    
 }
